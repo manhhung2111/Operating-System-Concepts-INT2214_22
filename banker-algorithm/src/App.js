b2c02ce73670d3b9fe8095ecdd9b02ff5bca4256
manh hung <manhhung2720@gmail.com>
2024-03-16 11:00:16 +0700

    Finish banker algorithm

--- banker-algorithm/src/App.js
+++ banker-algorithm/src/App.js
@@ -18,10 +18,12 @@ function App() {
   const [resources, setResources] = useState("");
   const [instances, setInstances] = useState([]);
   const [available, setAvailable] = useState([]);
+  const [solution, setSolution] = useState([]);
+  const [request, setRequest] = useState({ process: "", value: [] });
 
   const handleChangeData = (value, index, key, processId) => {
     const clone = _.cloneDeep(data);
-    clone[processId][key][index] = value;
+    clone[processId][key][index] = +value;
     setData((prev) => clone);
   };
 
@@ -30,6 +32,7 @@ function App() {
     const newInstances = Array(total).fill(0);
     setInstances((prev) => newInstances);
     setAvailable((prev) => newInstances);
+    setRequest((prev) => ({ ...prev, value: newInstances }));
     const clone = cloneDeep(data);
     clone.forEach((process) => {
       process.allocation = Array(total).fill(0);
@@ -66,7 +69,124 @@ function App() {
     setAvailable((prev) => availableClone);
   };
 
-  const handleReset = () => {};
+  const handleFindSequence = (available) => {
+    let work = [...available];
+    let finish = new Array(5).fill(false);
+    let safeSequence = [];
+
+    for (let i = 0; i < 5; i++) {
+      if (finish[i]) continue;
+      let processCanRun = true;
+      for (let j = 0; j < +resources; j++) {
+        if (data[i].need[j] > work[j]) {
+          processCanRun = false;
+          break;
+        }
+      }
+      if (processCanRun) {
+        const tmp = {
+          need: data[i].need,
+          work: [...work],
+          process: `P${i}`,
+        };
+        safeSequence.push(tmp);
+        for (let j = 0; j < +resources; j++) {
+          work[j] += +data[i].allocation[j];
+          work[j] = Math.min(work[j], +instances[j]);
+        }
+        finish[i] = true;
+        i = -1; // Restart the loop
+      }
+    }
+
+    // Check if all processes are finished
+    if (safeSequence.length === 5) {
+      setSolution((prev) => safeSequence);
+      return safeSequence;
+    } else {
+      setSolution((prev) => []);
+      alert("Deadlock!");
+      return null;
+    }
+  };
+
+  const requestProcess = () => {
+    // Check if the request is within the need
+    const processIndex = +request.process.split("P")[1];
+    for (let i = 0; i < +resources; i++) {
+      if (request.value[i] > data[processIndex].need[i]) {
+        alert("Request is not granted since request exceeds need");
+        return false;
+      }
+    }
+
+    // Check if the request is within the available resources
+    for (let i = 0; i < +resources; i++) {
+      if (request.value[i] > available[i]) {
+        alert(
+          "Request is not granted since there is not enough available resources"
+        );
+        return false; // Not enough available resources
+      }
+    }
+
+    // Simulate granting the request
+    const cloneAvailable = _.cloneDeep(available);
+    const cloneData = _.cloneDeep(data);
+    for (let i = 0; i < +resources; i++) {
+      cloneAvailable[i] -= request.value[i];
+      cloneData[processIndex].allocation[i] += +request.value[i];
+      cloneData[processIndex].need[i] -= +request.value[i];
+    }
+    setAvailable((prev) => cloneAvailable);
+    setData((prev) => cloneData);
+
+    let work = [...cloneAvailable];
+    let finish = new Array(5).fill(false);
+    let safeSequence = [];
+
+    for (let i = 0; i < 5; i++) {
+      if (finish[i]) continue;
+      let processCanRun = true;
+      for (let j = 0; j < +resources; j++) {
+        if (cloneData[i].need[j] > work[j]) {
+          processCanRun = false;
+          break;
+        }
+      }
+      if (processCanRun) {
+        const tmp = {
+          need: cloneData[i].need,
+          work: [...work],
+          process: `P${i}`,
+        };
+        safeSequence.push(tmp);
+        for (let j = 0; j < +resources; j++) {
+          work[j] += +cloneData[i].allocation[j];
+          work[j] = Math.min(work[j], +instances[j]);
+        }
+        finish[i] = true;
+        i = -1; // Restart the loop
+      }
+    }
+
+    // Check if all processes are finished
+    if (safeSequence.length === 5) {
+      setSolution((prev) => safeSequence);
+      return safeSequence;
+    } else {
+      setSolution((prev) => []);
+      alert("Deadlock!");
+      return null;
+    }
+  };
+
+  const handleChangeRequestValue = (value, index) => {
+    const clone = _.cloneDeep(request);
+    clone.value[index] = +value;
+    setRequest((prev) => clone);
+  };
+
   return (
     <div className="App">
       <h2 className="header">Banker's Algorithm Simulator</h2>
@@ -119,9 +239,16 @@ function App() {
               <Button
                 variant="warning"
                 className="button"
-                onClick={() => handleReset()}
+                onClick={() => handleFindSequence(available)}
               >
-                Reset
+                Find sequence
+              </Button>{" "}
+              <Button
+                variant="dark"
+                className="button"
+                onClick={() => requestProcess()}
+              >
+                Request resource
               </Button>{" "}
             </div>
           )}
@@ -136,33 +263,117 @@ function App() {
           />
         </div>
       )}
-      <div className="available">
-        <Table striped bordered hover className="table">
-          <thead>
-            <tr>
-              <th colSpan={+resources} style={{ textAlign: "center" }}>
-                Available
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              {data[0].allocation.map((value, index) => {
-                return (
-                  <td style={{ textAlign: "center" }}>
-                    {String.fromCharCode(index + "A".charCodeAt(0))}
+      {instances.length > 0 && (
+        <div className="available-request">
+          <div className="available">
+            <Table striped bordered hover className="table">
+              <thead>
+                <tr>
+                  <th colSpan={+resources} style={{ textAlign: "center" }}>
+                    Available
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  {data[0].allocation.map((value, index) => {
+                    return (
+                      <td style={{ textAlign: "center" }}>
+                        {String.fromCharCode(index + "A".charCodeAt(0))}
+                      </td>
+                    );
+                  })}
+                </tr>
+                <tr>
+                  {available.map((value) => {
+                    return <td>{value}</td>;
+                  })}
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <div className="request">
+            <Table striped bordered hover className="table">
+              <thead>
+                <tr>
+                  <th colSpan={+resources + 1} style={{ textAlign: "center" }}>
+                    Request resource
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Process Id</td>
+                  {data[0].allocation.map((value, index) => {
+                    return (
+                      <td style={{ textAlign: "center" }}>
+                        {String.fromCharCode(index + "A".charCodeAt(0))}
+                      </td>
+                    );
+                  })}
+                </tr>
+                <tr>
+                  <td className="input">
+                    <input
+                      type="text"
+                      value={request.process}
+                      onChange={(e) =>
+                        setRequest((prev) => ({
+                          ...prev,
+                          process: e.target.value,
+                        }))
+                      }
+                    />
                   </td>
-                );
-              })}
-            </tr>
-            <tr>
-              {available.map((value) => {
-                return <td>{value}</td>;
-              })}
-            </tr>
-          </tbody>
-        </Table>
-      </div>
+                  {request.value.map((value, index) => {
+                    return (
+                      <td className="input">
+                        <input
+                          type="text"
+                          value={value}
+                          onChange={(e) =>
+                            handleChangeRequestValue(e.target.value, index)
+                          }
+                        />
+                      </td>
+                    );
+                  })}
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      )}
+      {solution.length > 0 && (
+        <div>
+          <p>{`Safe sequence: ${solution[0].process}, ${solution[1].process}, ${solution[2].process}, ${solution[3].process}, ${solution[4].process},`}</p>
+          <p>{`Step 1: Since need[${
+            solution[0].process?.split("P")[1]
+          }] = (${JSON.stringify(solution[0].need)}) <= work = ${JSON.stringify(
+            solution[0].work
+          )}`}</p>
+          <p>{`Step 2: Since need[${
+            solution[1].process?.split("P")[1]
+          }] = (${JSON.stringify(solution[1].need)}) <= work = ${JSON.stringify(
+            solution[1].work
+          )}`}</p>
+          <p>{`Step 3: Since need[${
+            solution[2].process?.split("P")[1]
+          }] = (${JSON.stringify(solution[2].need)}) <= work = ${JSON.stringify(
+            solution[2].work
+          )}`}</p>
+          <p>{`Step 4: Since need[${
+            solution[3].process?.split("P")[1]
+          }] = (${JSON.stringify(solution[3].need)}) <= work = ${JSON.stringify(
+            solution[3].work
+          )}`}</p>
+          <p>{`Step 5: Since need[${
+            solution[4].process?.split("P")[1]
+          }] = (${JSON.stringify(solution[4].need)}) <= work = ${JSON.stringify(
+            solution[4].work
+          )}`}</p>
+        </div>
+      )}
     </div>
   );
 }
